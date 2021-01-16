317953cbcefa9d36335e9fc4be953a1565708594
= <=>
2021-01-16 14:59:40 +0530

    File added

---
+++ Assign/Cal-api/src/main/java/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public static void Addition(int number1 , int number2){
+        System.out.println("Addition is " + (number1+number2));
+    }
+    public static void Substraction(int number1 , int number2){
+        System.out.println("Substraction is " + (number1-number2));
+    }
+    public static void Multiplication(int number1 , int number2){
+        System.out.println("Multiplication is " + (number1*number2));
+    }
+    public static void Division(int number1 , int number2){
+        System.out.println("Division is " + (number1/number2));
+    }
+}
---
+++ Assign/Cal-impl/src/main/java/Application.java
@@ -0,0 +1,13 @@
+public class Application {
+    public static void main(String args[]){
+
+
+        Calculator cal = new Calculator();
+        cal.Addition(30,20);
+        cal.Substraction(30,20);
+        cal.Multiplication(30,20);
+        cal.Division(30,20);
+    }
+
+}
+
